03c21ad5057255ae279c53a664967307e574a45d
Vasil <vas4oo.vs@gmail.com>
2021-06-02 23:00:07 +0300

    maven project

---
+++ src/main/java/pu/soft/arch/GitHerokuDemo/GitHerokuDemoApplication.java
@@ -0,0 +1,13 @@
+package pu.soft.arch.GitHerokuDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHerokuDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHerokuDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pu/soft/arch/GitHerokuDemo/HelloController.java
@@ -0,0 +1,20 @@
+package pu.soft.arch.GitHerokuDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class HelloController {
+
+	@GetMapping(path="/")
+	public String retrieveGreeting() {
+		LocalDateTime currentDateTime = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+		String formattedDateTime = currentDateTime.format(dateTimeFormatter);
+		return "Hello the date and time is: "+ formattedDateTime;
+	}
+}
